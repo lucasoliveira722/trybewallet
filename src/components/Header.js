e8f3343b71d600accc55931827ad7474766d1200
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-01-18 19:34:34 -0300

    Requisito 4 completo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,46 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Header.css';
 
-export default class Header extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      totalExpenses: 0,
+class Header extends Component {
+    sumValuesExpenses = () => {
+      const { expenses } = this.props;
+      return expenses.reduce((acc, cur) => {
+        const cambio = cur.exchangeRates[cur.currency].ask;
+        return acc + (Number(cur.value) * cambio);
+      }, 0);
     };
-  }
 
-  render() {
-    const { email } = this.props;
-    const { totalExpenses } = this.state;
-    return (
-      <header>
-        <h3 data-testid="total-field">
-          { totalExpenses }
-        </h3>
-        <h3 data-testid="email-field">
-          { email }
-        </h3>
-        <h3 data-testid="header-currency-field">
-          <p> BRL </p>
-        </h3>
-      </header>
-    );
-  }
+    render() {
+      const { email } = this.props;
+      // console.log(expenses);
+      // const { totalExpenses } = this.state;
+      return (
+        <header>
+          <h3 data-testid="total-field">
+            {this.sumValuesExpenses().toFixed(2)}
+          </h3>
+          <h3 data-testid="email-field">
+            { email }
+          </h3>
+          <h3 data-testid="header-currency-field">
+            <p> BRL </p>
+          </h3>
+        </header>
+      );
+    }
 }
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.shape({
+    reduce: PropTypes.func.isRequired,
+  }).isRequired,
 };
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Header);
